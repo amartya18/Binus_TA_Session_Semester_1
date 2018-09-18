99a13a5a2f699e274aa09e4fb47d0e613685787f
Amartya Saleh Kadarisman <amartya@Amartyas-MacBook-Pro.local>
2018-09-18 21:53:06 +0700

    assignment 1

--- TA_assignment_stars.py
+++ TA_assignment_stars.py
@@ -1,12 +1,20 @@
 inp = int(input("input a number: "))
 for i in range (1, inp+1):
-    print(" " * (inp - i),("*" * i))
+    print("*" * i)
+print("")
 
-inp = int(input("input a number: "))
+for i in range (1, inp+1):
+    print(" " * (inp - i)+("*" * i))
+print("")
+
 for i in range (1, inp+1):
     print("*" * (inp + 1 - i))
+print("")
 
-inp = int(input("input a number: "))
+for i in range (0, inp):
+    print(" " * i+"*" * (inp - i))
+print("")
+
 nilai1 = int(1)
 nilai2 = int(inp-1)
 for i in range (1,inp+1):
@@ -15,10 +23,4 @@ for i in range (1,inp+1):
     print(" " * (x)+"*" * (y))
     nilai1 = nilai1 + 2
     nilai2 = (nilai2 - 2)
-
-
-
-
-
-
-
+print("")
